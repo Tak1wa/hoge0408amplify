ed77d95298eb243d9fbfa95e12b667fd4bffdc0a
Tak1wa <iwasa.takahito@classmethod.jp>
2025-04-08 02:11:01 +0900

    hoge

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,11 +3,23 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { hogeschedule } from './hogejob/hogeschedule/resource';
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
-defineBackend({
+import { CfnRule } from 'aws-cdk-lib/aws-events';
+import { Aspects, IAspect } from 'aws-cdk-lib';
+import { IConstruct } from 'constructs';
+
+const backend = defineBackend({
   auth,
   data,
   hogeschedule,
 });
+
+class DisableScheduleRules implements IAspect {
+  visit(node: IConstruct): void {
+    if (node instanceof CfnRule) {
+      if (node.node.path.includes('hogeschedule')) {
+        node.state = 'DISABLED';
+      }
+    }
+  }
+}
+Aspects.of(backend.stack).add(new DisableScheduleRules());
